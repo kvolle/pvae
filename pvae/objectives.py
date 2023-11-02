a745e3f92add15fcea9e855af9864d098b7a4f41
Kyle Volle <kyle.volle@gmail.com>
2023-11-02 15:34:52 -0500

    Changed imports to match current geoopt

--- pvae/objectives.py
+++ pvae/objectives.py
@@ -19,6 +19,7 @@ def vae_objective(model, x, K=1, beta=1.0, components=False, analytical_kl=False
     obj = -lpx_z.mean(0).sum() + beta * kld.mean(0).sum()
     return (qz_x, px_z, lpx_z, kld, obj) if components else obj
 
+
 def _iwae_objective_vec(model, x, K):
     """Helper for IWAE estimate for log p_\theta(x) -- full vectorisation."""
     qz_x, px_z, zs = model(x, K)
