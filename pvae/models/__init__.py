20639e0a0dab866b78b6f593cf009d74b3ed7b5c
Kyle Volle <kyle.volle@gmail.com>
2023-11-07 11:23:21 -0600

    Added Fashion MNIST as option

--- pvae/models/__init__.py
+++ pvae/models/__init__.py
@@ -1,4 +1,5 @@
 from .tabular import Tree as VAE_tree, CSV as VAE_csv
 from .mnist import Mnist as VAE_mnist
+from .fmnist import Fmnist as VAE_fmnist
 
-__all__ = [VAE_csv, VAE_tree, VAE_mnist]
+__all__ = [VAE_csv, VAE_tree, VAE_mnist, VAE_fmnist]
